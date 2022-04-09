b0807850e5e3ff61f864a960802120f9f47c8313
dominik1408 <dominik.kasprzak@PK1-12>
2022-04-09 15:54:29 +0200

    LINQ

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Linq
 {
@@ -6,7 +8,24 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj jakiś tekst");
+
+            string t = Console.ReadLine();
+            string[] text = t.Split(' ');
+
+            Dictionary<int, List<string>> dictionary = new Dictionary<int, List<string>>();
+
+            Console.WriteLine("-----------------------LINQ-------------------------");
+            text.GroupBy(x => x.Length).ToList().ForEach(x => {
+                Console.WriteLine(x.Key);
+                x.ToList().ForEach(y => Console.WriteLine(y));
+            });
+
+    
+            Console.WriteLine("-------------------------------------------------------");
+            int[] intTable = { 3, 4, 7, 1, 3, 4, 5, 7, 5, 5, 3, 2, 3, 4, 9, 1, 2, 3 };
+            intTable.Select(k => k * 2).ToList().ForEach(k => Console.WriteLine(k));
+            intTable.Where(x => x > 6).Select(x => x*2).OrderBy(x => x).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
